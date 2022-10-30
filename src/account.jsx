9c8b2284c621f8f89c975e219626455a3af80a10
Y Nasr <yoyo.monem22@gmail.com>
2022-10-30 21:00:47 +0100

    Initial commit

---
+++ src/account.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MdAccountCircle from "react-icons";
+const Account = () => {
+    return () => {
+        <div id="Account">Youssef <MdAccountCircle /></div>
+    }
+};
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Account from "account";
+const index = () => {
+    return () => {
+        <div id="index">
+            <Account />
+        </div>
+    };
+};
